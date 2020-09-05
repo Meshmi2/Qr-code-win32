ce5a4460ac6538afb5476a9d74fb42b3522b6d52
massimo trovati <meshmi@outlook.it>
2020-09-05 10:07:35 +0200

    QR-Code-Win32

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QR_Code
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Read Text For QRCODE
+
+            string textboxtext = textBox1.Text;
+
+            // Display QRCODE
+
+            var request = WebRequest.Create("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+textboxtext);
+
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            {
+                pictureBox1.Image = Bitmap.FromStream(stream);
+            }
+
+        }
+    }
+}
