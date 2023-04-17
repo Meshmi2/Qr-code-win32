8fa752a01dcd62737597cc9978bd56727fdeb03f
massimo trovati <meshmi@outlook.it>
2023-04-17 09:39:16 +0200

    new function
    save as png file is now aviable

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,22 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.Enabled = false;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 140);
+            this.button1.ForeColor = System.Drawing.Color.Coral;
+            this.button1.Location = new System.Drawing.Point(12, 102);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(360, 52);
             this.button1.TabIndex = 0;
             this.button1.Text = "GENERATE";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
@@ -58,10 +66,11 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 81);
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(360, 20);
             this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // pictureBox1
             // 
@@ -70,16 +79,34 @@
             this.pictureBox1.Size = new System.Drawing.Size(150, 150);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(298, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Click For Save The QR CODE";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 216);
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(569, 212);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "QR-CODE GENERATOR";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -94,6 +121,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,16 +8,23 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace QR_Code
 {
     public partial class Form1 : Form
     {
+        private readonly object stream1;
+        private readonly object image1;
+
         public Form1()
         {
             InitializeComponent();
         }
-
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = !string.IsNullOrEmpty(textBox1.Text);
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             // Read Text For QRCODE
@@ -26,14 +33,33 @@ namespace QR_Code
 
             // Display QRCODE
 
-            var request = WebRequest.Create("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+textboxtext);
-
+            var request = WebRequest.Create("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + textboxtext);
             using (var response = request.GetResponse())
             using (var stream = response.GetResponseStream())
             {
                 pictureBox1.Image = Bitmap.FromStream(stream);
+                System.Net.ServicePointManager.Expect100Continue = false;
             }
-
+            
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { 
+            // Save QRCODE .PNG
+            if (pictureBox1.Image != null)
+                    pictureBox1.Image = pictureBox1.Image;
+                saveFileDialog1.Filter = "PNG|*.png";
+                saveFileDialog1.ShowDialog();
+                if (saveFileDialog1.FileName != "")
+            {
+                    System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                    switch (saveFileDialog1.FilterIndex)
+                {
+                        case 1:
+                            this.pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                    }
+                    fs.Close();
+                }
         }
     }
 }
